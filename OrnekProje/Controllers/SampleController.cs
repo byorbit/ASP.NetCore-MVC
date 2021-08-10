ac01fc9612373bef47c72426b4a5f9c97af77216
fatih.metin <fatih.metin@FMETIN2>
2021-08-10 17:26:29 +0300

    form ile veri gönderme

---
+++ OrnekProje/Controllers/SampleController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrnekProje.Controllers
+{
+    public class SampleController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetSample()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateSample(IFormCollection datas)
+        {
+            var value1 = datas["txtValue1"];
+            var value2 = datas["txtValue2"];
+            var value3 = datas["txtValue3"];
+
+            return View();
+        }
+    }
+}
